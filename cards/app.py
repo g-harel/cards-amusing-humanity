8d078eeb69eaf50ed8d0bdc7f5c6dc8cd3fe3083
winterhart <m.user.work@gmail.com>
2019-03-12 20:21:08 -0400

    First Prototype docker, addition of waitress tech to serve flask application

--- cards/app.py
+++ cards/app.py
@@ -2,21 +2,23 @@ from flask import Flask
 from views import answers, questions, card_brewer, main
 from settings import DatabaseConfig
 from datasource.database import db
+from waitress import serve
 
+application = Flask(__name__)
 
 def create_app():
-    app = Flask(__name__)
+
     conf = DatabaseConfig()
-    app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = conf.get_db_uri()
-    db.init_app(app)
-    app.register_blueprint(answers.ans)
-    app.register_blueprint(questions.ques)
-    app.register_blueprint(card_brewer.brew)
-    app.register_blueprint(main.main)
-    return app
+    application.config['DEBUG'] = True
+    application.config['SQLALCHEMY_DATABASE_URI'] = conf.get_db_uri()
+    db.init_app(application)
+    application.register_blueprint(answers.ans)
+    application.register_blueprint(questions.ques)
+    application.register_blueprint(card_brewer.brew)
+    application.register_blueprint(main.main)
+    return application
 
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run()
+    application = create_app()
+    serve(application, host='0.0.0.0', port=8000)
