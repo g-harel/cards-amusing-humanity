5d8577d158c01848c634eb4a5017c46e625a5583
g-harel <gabrielj.harel@gmail.com>
2019-03-22 22:42:21 -0400

    add sign route to signing service

--- signing/main.py
+++ signing/main.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from datetime import datetime, timedelta
+
+from flask import Flask, request, jsonify, make_response
+import jwt
+
+# TODO from env
+key = "abcde"
+exp = timedelta(hours=1)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+def error_response(code, msg):
+    return make_response(jsonify({"error": msg}), code)
+
+
+@app.route("/sign", methods=["POST"])
+def sign():
+    body = request.get_json()
+    if (not body):
+        return error_response(400, "No data")
+
+    payload = body.get("payload")
+    if (not payload) or (not type(payload) is dict):
+        return error_response(400, "Malformed data")
+
+    payload["exp"] = int(datetime.timestamp(datetime.now() + exp))
+    token = jwt.encode(payload, key, algorithm="HS256")
+    return jsonify({"token": token.decode("utf-8")})
+
+
+@app.route("/verify")
+def verify():
     return "signing service"
+
 
 if __name__ == "__main__":
     app.run()
