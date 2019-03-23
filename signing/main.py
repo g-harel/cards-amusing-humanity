4b716c08557cfe9e63d9622198b2027962151658
g-harel <gabrielj.harel@gmail.com>
2019-03-23 16:02:07 -0400

    implement verify route in signing service

--- signing/main.py
+++ signing/main.py
@@ -9,8 +9,14 @@ exp = timedelta(hours=1)
 
 app = Flask(__name__)
 
+
 def error_response(code, msg):
     return make_response(jsonify({"error": msg}), code)
+
+
+@app.errorhandler(500)
+def exception():
+    return error_response(500, "Internal error")
 
 
 @app.route("/sign", methods=["POST"])
@@ -21,16 +27,28 @@ def sign():
 
     payload = body.get("payload")
     if (not payload) or (not type(payload) is dict):
-        return error_response(400, "Malformed data")
+        return error_response(400, "Malformed payload")
 
     payload["exp"] = int(datetime.timestamp(datetime.now() + exp))
     token = jwt.encode(payload, key, algorithm="HS256")
     return jsonify({"token": token.decode("utf-8")})
 
 
-@app.route("/verify")
+@app.route("/verify", methods=["POST"])
 def verify():
-    return "signing service"
+    body = request.get_json()
+    if (not body):
+        return error_response(400, "No data")
+
+    token = body.get("token")
+    if (not token) or (not type(token) is str):
+        return error_response(400, "Malformed token")
+
+    try:
+        jwt.decode(token, key, algorithms=['HS256'])
+        return "Valid token"
+    except:
+        return error_response(401, "Expired or malformed token")
 
 
 if __name__ == "__main__":
