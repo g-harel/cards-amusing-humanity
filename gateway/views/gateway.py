a363542e0b62de3fdc59380d245898287f40518e
winterhart <m.user.work@gmail.com>
2019-04-20 00:38:55 -0400

    IP getter mechanism

--- gateway/views/gateway.py
+++ gateway/views/gateway.py
@@ -1,18 +1,33 @@
 import redis
+import os
 from flask import Blueprint, request
 
 gate = Blueprint('gate', __name__, url_prefix='/api')
 
 # mem = redis.Redis(host="gateway-redis", port=6379, db=0)
 
+# RATE_LIMIT  = int(float(os.getenv("RATE-LIMITING-THRESHOLD")))
+# TIMEOUT = int(float(os.getenv("BLOCKED-IP-TIMEOUT")))
+
+RATE_LIMIT = 3
+TIMEOUT = 5
 
 # Grabing the IP from user's request
 def authorize_request(Request):
-    print(Request.headers)
-    print("IP")
-    print(Request.remote_addr)
-    print("IP 2")
-    print(request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+    IP = None
+    try:
+        forwarded_ip = Request.environ.get('HTTP_X_FORWARDED_FOR')
+        if forwarded_ip is None:
+            IP = Request.environ.get('REMOTE_ADDR')
+        else:
+            IP = request.environ.get('HTTP_X_FORWARDED_FOR')
+    except:
+        print("Problem while getting IP")
+        return True
+
+    #TODO: Refresh DB, remove expired rate
+    #TODO: Save the new IP entry or increment one
+    #TODO Get new count and decide to block or allow
     return True
 
 
