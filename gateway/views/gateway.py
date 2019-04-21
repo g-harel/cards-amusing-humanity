cabf0b9a910e59768cd46b5b2db0e6fd60228b0f
winterhart <m.user.work@gmail.com>
2019-04-20 22:10:09 -0400

    Adding all logic for IP rate limiting, adding variable for timeout and ratio of request

--- gateway/views/gateway.py
+++ gateway/views/gateway.py
@@ -1,26 +1,23 @@
 import redis
 import os
 import logging
-import datetime
+import time
 import ast
 import requests
 from flask import Blueprint, jsonify, request, make_response
 
 gate = Blueprint('gate', __name__, url_prefix='/api')
 
-# memdb = redis.Redis(host="127.0.0.1", port=6379, db=0)
+memdb = redis.Redis(host="gateway-redis", port=6379, db=0)
 
-# RATE_LIMIT  = int(float(os.getenv("RATE-LIMITING-THRESHOLD")))
-# TIMEOUT = int(float(os.getenv("BLOCKED-IP-TIMEOUT")))
+RATE_LIMIT  = int(os.getenv("RATE_LIMITING_THRESHOLD"))
+TIMEOUT = int(os.getenv("BLOCKED_IP_TIMEOUT"))
 
-RATE_LIMIT = 3
-TIMEOUT = 5
 gunicorn_logger = logging.getLogger('gunicorn.error')
 
 # Grabing the IP from user's request
 def authorize_request(Request):
     IP = None
-    return True
     try:
         forwarded_ip = Request.environ.get('HTTP_X_FORWARDED_FOR')
         if forwarded_ip is None:
@@ -32,28 +29,47 @@ def authorize_request(Request):
         return True
 
     gunicorn_logger.info('IP Address: ' + str(IP))
-    try:
-        IP_data = memdb.hmget(IP)
-    except:
-        gunicorn_logger("New IP client")
-    
-    
-    if IP_data is None:
-        # Case where it's a new IP
-        data = {
-            'IP': IP,
-            'Counter' : 0,
-            'Date': datetime.datetime.now()
-        }
-        memdb.hmset(IP, data)
-        return True
-    
-    # Case where it's same IP again
-    gunicorn_logger.info('from db ' + str(IP_data))
 
-    #TODO: Refresh DB, remove expired rate
-    #TODO: Save the new IP entry or increment one
-    #TODO Get new count and decide to block or allow
+    IP_data = None
+    IP = str(IP)
+    try:
+        gunicorn_logger.info('get data')
+        IP_data = memdb.hmget(IP, ['ip', 'counter', 'last_transaction'])
+
+    except Exception as error:
+        gunicorn_logger.info(error)
+    
+    
+    if IP_data[0] is None:
+        # Case where it's a new IP
+        date_n = int(time.time())
+        memdb.hmset(IP, {'ip': IP, 'counter': 0, 'last_transaction': date_n})
+        return True
+
+
+    gunicorn_logger.info("Known Client !!!")
+
+    # Case where it's same IP again
+    previous_count = int(IP_data[1])
+    new_count = 0
+    if previous_count > RATE_LIMIT:
+        # Case where the user used the service too mutch
+        threshold_datetime = int(IP_data[2])
+        threshold_datetime = threshold_datetime +  (60*TIMEOUT)
+        if int(time.time()) > threshold_datetime:
+            # Reset Coutner and allow user again
+            gunicorn_logger.info("Reset Counter")
+            new_count = 0
+        else:
+            # Block transaction 
+            gunicorn_logger.info("Blocking transaction")
+            return False
+    else:
+        new_count = int(previous_count)  + 1
+    
+    gunicorn_logger.info("Increment Counter")
+    date_now = int(time.time())
+    memdb.hmset(IP, {'ip': IP, 'counter': new_count, 'last_transaction': date_now})
     return True
 
 
