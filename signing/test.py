56db102822298a4416a346b9398b9c4e21af78d9
g-harel <gabrielj.harel@gmail.com>
2019-04-23 19:49:30 -0400

    add test to decode signed payload

--- signing/test.py
+++ signing/test.py
@@ -1,6 +1,8 @@
 import unittest
 import json
 
+import jwt
+
 from main import app as tested_app
 
 
@@ -10,7 +12,7 @@ class TestApp(unittest.TestCase):
 
 
     def test_sign_success(self):
-        data = {"payload": {"a": 0}}
+        data = {"payload": {"test_sign_success": ""}}
         response = self.app.post("/sign", data=json.dumps(data), content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
@@ -19,7 +21,19 @@ class TestApp(unittest.TestCase):
 
 
     def test_verify_success(self):
-        data = {"payload": {"b": 0}}
+        data = {"payload": {"test_verify_success": ""}}
         sign_response = self.app.post("/sign", data=json.dumps(data), content_type="application/json")
+
         verify_response = self.app.post("/verify", data=str(sign_response.data, "utf8"), content_type="application/json")
+
         self.assertEqual(verify_response.status_code, 200)
+
+
+    def test_verify_sign_contains_payload(self):
+        data = {"payload": {"test_verify_sign_contains_payload": ""}}
+        response = self.app.post("/sign", data=json.dumps(data), content_type="application/json")
+
+        token = json.loads(response.data).get("token")
+        payload = jwt.decode(token, verify=False, algorithms=["HS256"])
+
+        self.assertDictContainsSubset(data["payload"], payload)
