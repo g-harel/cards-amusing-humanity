520ca2700249c3380106fa6dd74b9522e65cacc5
winterhart <m.user.work@gmail.com>
2019-04-25 22:06:04 -0400

    Testing two cases with the rate limiting API

--- gateway/test/test_app.py
+++ gateway/test/test_app.py
@@ -1,22 +1,69 @@
 import os
+import flask
 from unittest import mock
-from ..memory.storage import Storage
-from ..views.gateway import authorize_request
+
 
 @mock.patch.dict(os.environ, {'RATE_LIMITING_THRESHOLD': '3'})
 @mock.patch.dict(os.environ, {'BLOCKED_IP_TIMEOUT': '3'})
-def test_authorize():
+def test_authorize_success():
     # print os.environ
+    from ..views.gateway import authorize_request
+    req = mock.patch.object(flask, "request")
+    environ = {'REMOTE_ADDR': '12.12.12.12'}
+    req.__setattr__('environ', environ)
+    storage = fakeStorage
+    authorize_request(req, storage)
+    authorize_request(req, storage)
+    assert authorize_request(req, storage)
 
 
-    fake_Request = {
-        'REMOTE_ADDR': '12.12.12.3'
-    }
-    authorize_request(fake_Request, Storage)
-    authorize_request(fake_Request, Storage)
-    assert authorize_request(fake_Request, Storage)
+@mock.patch.dict(os.environ, {'RATE_LIMITING_THRESHOLD': '2'})
+@mock.patch.dict(os.environ, {'BLOCKED_IP_TIMEOUT': '3'})
+def test_authorize_failure():
+    # print os.environ
+    from ..views.gateway import authorize_request
+    req = mock.patch.object(flask, "request")
+    environ = {'REMOTE_ADDR': '12.12.12.12'}
+    req.__setattr__('environ', environ)
+    storage = fakeStorage
+    authorize_request(req, storage)
+    authorize_request(req, storage)
+    authorize_request(req, storage)
+    authorize_request(req, storage)
+    assert not authorize_request(req, storage)
 
 
+class fakeStorage():
+    memory = []
+    def get(self, key):
+        """ Get data using key """
+        try:
+            for i, row in enumerate(self.memory):
+                if key == row[0]:
+                    return [row[0], row[1], row[2]]
+        except Exception as error:
+            print(error)
 
+        return ['new', None, 'mwmee']
 
-class 
+    def set(self, key, data):
+        """ Set Data  """
+        try:
+            print(data['counter'])
+            for i, row in enumerate(self.memory):
+                if key == row[0]:
+                    self.memory.remove(row)
+
+            self.memory.append([data['ip'], data['counter'], data['last_transaction']])
+        except Exception as error:
+            print(error)
+
+    def exists(self, key):
+        """ Exists  """
+        exist = False
+        try:
+            super
+        except Exception as error:
+            print(error)
+        return exist
+
